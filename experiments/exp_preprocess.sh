4326dafc2f304fe4957f651b5d9ec373fe778da6
valleysprings <377206338@qq.com>
2025-05-28 23:47:13 +0800

    some changes on experiments script

--- experiments/exp_preprocess.sh
+++ experiments/exp_preprocess.sh
@@ -1,10 +1,10 @@
 # preprocess
-# python ../auxiliary/preprocess_nofeat.py --dataset amazon
-# python ../auxiliary/preprocess_nofeat.py --dataset youtube
-# python ../auxiliary/preprocess_nofeat.py --dataset dblp
-# python ../auxiliary/preprocess_nofeat.py --dataset lj
+python ../auxiliary/preprocess_nofeat.py --dataset amazon
+python ../auxiliary/preprocess_nofeat.py --dataset youtube
+python ../auxiliary/preprocess_nofeat.py --dataset dblp
+python ../auxiliary/preprocess_nofeat.py --dataset lj
 
-# python ../auxiliary/preprocess_small.py --dataset dolphins
-# python ../auxiliary/preprocess_small.py --dataset karate
-# python ../auxiliary/preprocess_small.py --dataset eu-core
-# python ../auxiliary/preprocess_small.py --dataset football
+python ../auxiliary/preprocess_small.py --dataset dolphins
+python ../auxiliary/preprocess_small.py --dataset karate
+python ../auxiliary/preprocess_small.py --dataset eu-core
+python ../auxiliary/preprocess_small.py --dataset football
